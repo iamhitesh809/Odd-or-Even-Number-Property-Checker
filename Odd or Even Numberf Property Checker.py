8eb5c6d4263ae2abbd4dcb4d16c23ac201fbca87
iamhitesh809 <112393651+iamhitesh809@users.noreply.github.com>
2022-10-15 00:42:51 +0530

    Add files via upload

---
+++ Odd or Even Numberf Property Checker.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Odd or Even Numberf Property Checker
+
+# In[2]:
+
+
+number = int(input("Which number you want to check? "))
+if number%2 == 1:
+    print("Number is Odd")
+else:
+    print("Number is Even")
+
